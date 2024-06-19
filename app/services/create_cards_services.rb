131fdd057374b549904ff9d6898f87c24c05120b
Heber Lazo <heberlazo@gmail.com>
2024-06-18 22:39:47 -0500

    Added files to gitignore

--- app/services/create_cards_services.rb
+++
@@ -1,8 +0,0 @@
-class CreateCardsServices
-  def self.perform
-    csv_file_path = 'app/assets/sheets/superdeckdiego.csv'
-    CSV.foreach(csv_file_path, headers: true, col_sep: ',') do |row|
-      Card.create!(name: row['NAME'])
-    end
-  end
-end
